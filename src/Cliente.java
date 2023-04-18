4a7707d21844789e9af787c1a9f2f7e8ada12dae
iZezito <emersonsilva81240@gmail.com>
2023-04-18 20:28:50 -0300

    legal 5.0

--- src/Cliente.java
+++ src/Cliente.java
@@ -7,22 +7,26 @@ import java.net.Socket;
 
 public class Cliente {
     public static void main(String[] args) {
+        // Declaração de variáveis
         long startTime, endTime, tcpTime, udpTime;
         int[] numeros = { 1024, 10024, 20024, 30024, 40024, 50024, 60024};
 
+        // Loop sobre o array de inteiros
         for(int dataSize : numeros){
             System.out.println("Tamanho da mensagem: " + dataSize);
+
             // Envia dados via TCP
             try (Socket socket = new Socket("localhost", 1234)) {
                 OutputStream out = socket.getOutputStream();
                 byte[] buffer = new byte[dataSize];
-                startTime = System.nanoTime();
-                out.write(buffer);
-                endTime = System.nanoTime();
-                tcpTime = endTime - startTime;
+                startTime = System.nanoTime(); // Obtemos o tempo de início da transmissão
+                out.write(buffer); // Escrevemos os dados no output stream
+                endTime = System.nanoTime(); // Obtemos o tempo de término da transmissão
+                tcpTime = endTime - startTime; // Calculamos o tempo de transmissão
                 System.out.println("TCP transfer rate: " + (double)dataSize / tcpTime * 1000000000 + " bytes/sec");
-                // converta para segundos
+                // Calcula a taxa de transferência em bytes/segundo e exibe na tela
                 System.out.println("Tempo de envio TCP: " + tcpTime / 1000000000.0 + " segundos");
+                // Exibe o tempo de envio em segundos
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -31,18 +35,18 @@ public class Cliente {
             try (DatagramSocket socket = new DatagramSocket()) {
                 byte[] buffer = new byte[dataSize];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getLocalHost(), 5678);
-                startTime = System.nanoTime();
-                socket.send(packet);
-                endTime = System.nanoTime();
-                udpTime = endTime - startTime;
+                startTime = System.nanoTime(); // Obtemos o tempo de início da transmissão
+                socket.send(packet); // Enviamos o pacote através do socket UDP
+                endTime = System.nanoTime(); // Obtemos o tempo de término da transmissão
+                udpTime = endTime - startTime; // Calculamos o tempo de transmissão
                 System.out.println();
                 System.out.println("UDP transfer rate: " + (double)dataSize / udpTime * 1000000000 + " bytes/sec");
+                // Calcula a taxa de transferência em bytes/segundo e exibe na tela
                 System.out.println("Tempo de envio UDP: " + udpTime / 1000000000.0 + " segundos");
+                // Exibe o tempo de envio em segundos
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-
     }
 }
