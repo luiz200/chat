41577affdac3e1a38c9585c2ea9c8980a1c84eb0
luizs <luiz.souza.709@ufrn.edu.br>
2023-04-18 19:05:40 -0300

    legal 3.0

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -3,34 +3,41 @@ import java.net.*;
 
 public class ChatClient {
     public static void main(String[] args) throws IOException {
+        // Cria um socket que se conecta ao servidor na porta 5000
         Socket socket = new Socket("localhost", 5000);
         System.out.println("Conectado ao servidor");
 
+        // Cria um BufferedReader para receber mensagens do servidor
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // Cria um PrintWriter para enviar mensagens ao servidor
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        // Cria um BufferedReader para ler entradas do usuário pelo console
         BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
 
-        // Solicita o nome do cliente
+        // Solicita o nome do cliente ao usuário pelo console
         System.out.print("Digite seu nome: ");
         String name = consoleIn.readLine();
         out.println(name); // Envia o nome do cliente para o servidor
 
+        // Cria uma nova thread para receber mensagens do servidor em tempo real
         new Thread(() -> {
             try {
                 String serverInput;
                 while ((serverInput = in.readLine()) != null) {
-                    System.out.println(serverInput);
+                    System.out.println(serverInput); // Imprime a mensagem recebida do servidor
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
 
+        // Aguarda por entradas do usuário pelo console e envia-as ao servidor
         String userInput;
         while ((userInput = consoleIn.readLine()) != null) {
-            out.println(userInput); // Adiciona o nome do cliente à mensagem
+            out.println(userInput); // Adiciona o nome do cliente à mensagem e envia ao servidor
         }
 
+        // Fecha o socket ao finalizar o programa
         socket.close();
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -3,44 +3,72 @@ import java.net.*;
 import java.util.*;
 
 public class ChatServer {
+    // Cria um conjunto de objetos PrintWriter, que serão responsáveis por enviar mensagens para os clientes
     private static Set<PrintWriter> clients = new HashSet<>();
+
+    // Cria um mapa que associa o nome do cliente ao seu respectivo objeto PrintWriter
     private static Map<String, PrintWriter> nameToClient = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
+        // Cria um objeto ServerSocket que irá ouvir a porta 5000
         ServerSocket serverSocket = new ServerSocket(5000);
         System.out.println("Servidor iniciado na porta 5000");
 
+        // Loop infinito que aguarda a conexão de novos clientes
         while (true) {
+            // Aceita a conexão do cliente e cria um novo objeto Socket para se comunicar com ele
             Socket clientSocket = serverSocket.accept();
             System.out.println("Cliente conectado");
 
+            // Cria um objeto BufferedReader para receber mensagens do cliente
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            // Cria um objeto PrintWriter para enviar mensagens para o cliente
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
+            // Lê o nome do cliente enviado pelo BufferedReader e imprime uma mensagem de boas-vindas para ele
             String name = in.readLine();
             System.out.println(name + " entrou no chat");
-            out.println(name + "");
+            out.println("Bem-vindo(a) ao chat, " + name + "!");
 
+            // Percorre todos os clientes conectados e envia uma mensagem informando que um novo cliente entrou no chat
             for (PrintWriter client : clients) {
                 client.println(name + " entrou no chat");
             }
 
+            // Adiciona o objeto PrintWriter do novo cliente ao conjunto de clientes e ao mapa nameToClient
             clients.add(out);
             nameToClient.put(name, out);
 
+            // Cria uma nova thread para receber as mensagens do cliente
             new Thread(() -> {
                 try {
                     String inputLine;
                     while ((inputLine = in.readLine()) != null) {
-                        System.out.println(name + ": " + inputLine);
-                        for (PrintWriter client : clients) {
-                            client.println(name + ": " + inputLine);
+                        if (inputLine.startsWith("/exit")) {
+                            out.println("Você saiu do chat");
+                            clientSocket.close(); // Encerra a conexão com o cliente
+                        } else {
+                            // Imprime a mensagem recebida juntamente com o nome do cliente que a enviou
+                            System.out.println(name + ": " + inputLine);
+
+                            // Percorre todos os clientes conectados e envia a mensagem recebida juntamente com o nome do cliente que a enviou
+                            for (PrintWriter client : clients) {
+                                client.println(name + ": " + inputLine);
+                            }
                         }
+
                     }
+
                 } catch (IOException e) {
+                    // Se ocorrer uma exceção, imprime uma mensagem informando que o cliente saiu do chat
                     System.out.println(name + " saiu do chat");
+
+                    // Remove o objeto PrintWriter do cliente do conjunto de clientes e do mapa nameToClient
                     clients.remove(out);
                     nameToClient.remove(name);
+
+                    // Percorre todos os clientes conectados e envia uma mensagem informando que o cliente saiu do chat
                     for (PrintWriter client : clients) {
                         client.println(name + " saiu do chat");
                     }
